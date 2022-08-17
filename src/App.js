b97ec9b0debdb4819df83febfcd87d3dd766416e
Bachir <bachir@Bachirs-MacBook-Pro.local>
2022-08-17 00:10:25 -0400

    added generateNewDie function so that when the id clicked and when click the enroll button held dice will not rolled

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import Die from './components/Die';
 
   const [dice, setDice] = React.useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+}
+
+
   function allNewDice() {
     const arrDice = [];
     for (let i = 0; i < 10; i++) {
-      arrDice.push({
-        value: Math.floor(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
+      arrDice.push(generateNewDie())
     }
     return arrDice;
   }
@@ -23,7 +28,11 @@ import Die from './components/Die';
   const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
   function rollDice() {
-    setDice(allNewDice)
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? 
+          die :
+          generateNewDie()
+  }))
   }
 
   function holdDice(id) {
